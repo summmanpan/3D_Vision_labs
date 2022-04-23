dd56ee26631b7e77d17c54d47adc28504c216f9a
c2207 <102225573+cstil0@users.noreply.github.com>
2022-04-23 13:22:21 +0200

    Interactive metho done

--- lab1/utils.py
+++ lab1/utils.py
@@ -22,9 +22,7 @@ def line_draw(line, canv, size, color=(50,255,50)):
     canv.line([beg, end], width=4, fill=color)
 
 
-def plot_img(img, do_not_use=[0],plot_flag=False):
+def plot_img(img, do_not_use=[0]):
     plt.figure(do_not_use[0])
     do_not_use[0] += 1
-    if plot_flag:
-        plt.imshow(img)
-    return img
+    plt.imshow(img)
